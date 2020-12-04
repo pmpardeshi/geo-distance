6706f9ab8aff35e76a63268e2187c539a04ad63b
pmpardeshi <pramodmpardeshi@gmail.com>
2020-12-04 17:23:08 +0530

    created library

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import find_packages, setup
 setup(
-    name=’geodistlib’,
-    packages=find_packages(),
-    version=’0.1.0',
-    description=’Python library to find distance between two geo coordinates’,
-    author=’Pramod Pardeshi’,
-    license=’MIT’,
+    name='geodistlib',
+    packages=find_packages(include=['geodistlib']),
+    version='0.1.0',
+    description='Python library to find distance between two geo coordinates',
+    author='Pramod Pardeshi',
+    license='MIT',
+    install_requires=['numpy'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
 )
